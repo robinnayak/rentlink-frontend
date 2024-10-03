bad614435e3a203e8805cc52a662738864273c43
robinnayak <robinnayak86@gmail.com>
2024-10-03 14:13:08 +0545

    ...

--- src/component/api/base.js
+++ src/component/api/base.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000"
-// export const BASE_URL = "https://rentlink.up.railway.app"
+// export const BASE_URL = "http://localhost:8000"
+export const BASE_URL = "https://rentlink.up.railway.app"
 
 export const MEDIA_URL = "https://res.cloudinary.com/dga4svmpl/"
