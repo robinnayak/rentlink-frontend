17e5a57835dd187eb38c72fcf32828c9320829ed
robinnayak <robinnayak86@gmail.com>
2024-10-30 09:55:34 +0545

    cookies added so the user refreshed but still remain logged in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import "./App.css";
 import store from "./component/app/store";
 import NavigationContainer from "./component/navigation/NavigationContainer";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { setToken, setUser } from "./component/app/feature/authSlice";
+
+function AppContent() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    const email = Cookies.get("email");
+    const is_landowner = Cookies.get("is_landowner")
+    if(token && email){
+      dispatch(setToken(token));
+      dispatch(setUser({email: email}));
+      dispatch(setUser({is_landowner:is_landowner}))
+    }
+  }, [dispatch]);
+  return <NavigationContainer />;
+}
+
 function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer/>
+      <AppContent />
     </Provider>
   );
 }
--- src/component/authPages/Login.jsx
+++ src/component/authPages/Login.jsx
@@ -36,10 +36,12 @@ const Login = () => {
       // Dispatch action to store token in Redux
       dispatch(setToken(res.access));
       dispatch(setUser(res.user));
+      dispatch(setUser({is_landowner:res.user.is_landowner}))
 
       // Store token and email in cookies with expiration of 7 days
       Cookies.set("token", res.access, { expires: 7 });
       Cookies.set("email", res.user.email, { expires: 7 });
+      Cookies.set("is_landowner", res.user?.is_landowner, { expires: 7 });
 
       // Navigate to the home page after successful login
       navigate("/");
@@ -56,61 +58,61 @@ const Login = () => {
     <>
       <Navbar />
 
-    <div className="flex justify-center items-center h-screen bg-primary-bg pt-24">
-      <form
-        onSubmit={submitLogin}
-        className="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md"
-      >
-        <h1 className="text-2xl font-bold mb-6 text-center text-primary-text">
-          Login
-        </h1>
-        <div className="mb-4">
-          <label className="block text-primary-text text-sm font-bold mb-2">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-primary-text leading-tight focus:outline-none focus:shadow-outline"
-            required
-          />
-        </div>
-        <div className="mb-6">
-          <label className="block text-primary-text text-sm font-bold mb-2">
-            Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-primary-text leading-tight focus:outline-none focus:shadow-outline"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          className={`bg-primary-btn hover:bg-primary-btn-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full ${
-            isLoading ? "opacity-50 cursor-not-allowed" : ""
-          }`}
-          disabled={isLoading}
+      <div className="flex justify-center items-center h-screen bg-primary-bg pt-24">
+        <form
+          onSubmit={submitLogin}
+          className="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md"
         >
-          {isLoading ? "Logging in..." : "Login"} {/* Show loading text */}
-        </button>
-        <ApiErrors error={error} />
-        <div className="text-center mt-4">
-          <p className="text-primary-text">
-            Don't have an account?{" "}
-            <Link to="/register" className="text-primary-btn hover:underline">
-              Register here
-            </Link>
-          </p>
-        </div>
-      </form>
-      {/* Display error message if any */}
-    </div>
+          <h1 className="text-2xl font-bold mb-6 text-center text-primary-text">
+            Login
+          </h1>
+          <div className="mb-4">
+            <label className="block text-primary-text text-sm font-bold mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-primary-text leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-primary-text text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-primary-text leading-tight focus:outline-none focus:shadow-outline"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className={`bg-primary-btn hover:bg-primary-btn-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full ${
+              isLoading ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            disabled={isLoading}
+          >
+            {isLoading ? "Logging in..." : "Login"} {/* Show loading text */}
+          </button>
+          <ApiErrors error={error} />
+          <div className="text-center mt-4">
+            <p className="text-primary-text">
+              Don't have an account?{" "}
+              <Link to="/register" className="text-primary-btn hover:underline">
+                Register here
+              </Link>
+            </p>
+          </div>
+        </form>
+        {/* Display error message if any */}
+      </div>
     </>
   );
 };
